ea6ed575b815460a523d0f64e03ab182e1975945
Jacek Fedorynski <jfedor@jfedor.org>
2023-12-13 20:19:50 +0100

    Add build for Waveshare RP2040-PiZero
    https://www.waveshare.com/rp2040-pizero.htm

---
+++ firmware/src/boards/waveshare_rp2040_pizero.h
@@ -0,0 +1,31 @@
+// -----------------------------------------------------
+// NOTE: THIS HEADER IS ALSO INCLUDED BY ASSEMBLER SO
+//       SHOULD ONLY CONSIST OF PREPROCESSOR DIRECTIVES
+// -----------------------------------------------------
+
+#ifndef _BOARDS_WAVESHARE_RP2040_PIZERO_H
+#define _BOARDS_WAVESHARE_RP2040_PIZERO_H
+
+#define WAVESHARE_RP2040_PIZERO_BOARD
+
+#define GPIO_VALID_PINS_BASE 0b00111111111111111111111111111111
+
+#define PICO_DEFAULT_PIO_USB_DP_PIN 6
+
+// --- FLASH ---
+
+#define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
+
+#ifndef PICO_FLASH_SPI_CLKDIV
+#define PICO_FLASH_SPI_CLKDIV 2
+#endif
+
+#ifndef PICO_FLASH_SIZE_BYTES
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
+#endif
+
+#ifndef PICO_RP2040_B0_SUPPORTED
+#define PICO_RP2040_B0_SUPPORTED 1
+#endif
+
+#endif
