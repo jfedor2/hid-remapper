43477ab7008f5975135c4daf6bf12af9fab4e21e
Jacek Fedorynski <jfedor@jfedor.org>
2024-02-13 18:08:05 +0100

    Move serial pins to board definition

--- firmware/src/serial.h
+++ firmware/src/serial.h
@@ -5,20 +5,20 @@
 
 #define SERIAL_MAX_PAYLOAD_SIZE 512
 
-#ifdef HID_REMAPPER_BOARD
-
-#define SERIAL_TX_PIN 24
-#define SERIAL_RX_PIN 25
-#define SERIAL_CTS_PIN 26
-#define SERIAL_RTS_PIN 23
-
-#else
-
+#ifndef SERIAL_TX_PIN
 #define SERIAL_TX_PIN 20
-#define SERIAL_RX_PIN 21
-#define SERIAL_CTS_PIN 26
-#define SERIAL_RTS_PIN 27
+#endif
 
+#ifndef SERIAL_RX_PIN
+#define SERIAL_RX_PIN 21
+#endif
+
+#ifndef SERIAL_CTS_PIN
+#define SERIAL_CTS_PIN 26
+#endif
+
+#ifndef SERIAL_RTS_PIN
+#define SERIAL_RTS_PIN 27
 #endif
 
 typedef bool (*msg_recv_cb_t)(const uint8_t* data, uint16_t len);
