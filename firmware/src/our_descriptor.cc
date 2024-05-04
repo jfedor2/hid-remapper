e9255d9b859f6c3422322381710fe948f31f26b4
Jacek Fedorynski <jfedor@jfedor.org>
2024-05-04 18:56:00 +0200

    Make "Switch gamepad" emulate Horipad
    The difference is trivial, but some things have the Horipad's VID/PID
    hardcoded, which means that we should now be getting the correct
    button/axis mapping in places like Chrome and Steam.

--- firmware/src/our_descriptor.cc
+++ firmware/src/our_descriptor.cc
@@ -267,7 +267,7 @@ const uint8_t our_report_descriptor_absolute[] = {
     0xC0,                      // End Collection
 };
 
-const uint8_t our_report_descriptor_gamepad[] = {
+const uint8_t our_report_descriptor_horipad[] = {
     0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
     0x09, 0x05,        // Usage (Game Pad)
     0xA1, 0x01,        // Collection (Application)
@@ -276,11 +276,13 @@ const uint8_t our_report_descriptor_gamepad[] = {
     0x35, 0x00,        //   Physical Minimum (0)
     0x45, 0x01,        //   Physical Maximum (1)
     0x75, 0x01,        //   Report Size (1)
-    0x95, 0x10,        //   Report Count (16)
+    0x95, 0x0E,        //   Report Count (14)
     0x05, 0x09,        //   Usage Page (Button)
     0x19, 0x01,        //   Usage Minimum (0x01)
-    0x29, 0x10,        //   Usage Maximum (0x10)
+    0x29, 0x0E,        //   Usage Maximum (0x0E)
     0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x95, 0x02,        //   Report Count (2)
+    0x81, 0x01,        //   Input (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0x05, 0x01,        //   Usage Page (Generic Desktop Ctrls)
     0x25, 0x07,        //   Logical Maximum (7)
     0x46, 0x3B, 0x01,  //   Physical Maximum (315)
@@ -301,13 +303,9 @@ const uint8_t our_report_descriptor_gamepad[] = {
     0x75, 0x08,        //   Report Size (8)
     0x95, 0x04,        //   Report Count (4)
     0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x06, 0x00, 0xFF,  //   Usage Page (Vendor Defined 0xFF00)
-    0x09, 0x20,        //   Usage (0x20)
+    0x75, 0x08,        //   Report Size (8)
     0x95, 0x01,        //   Report Count (1)
-    0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x0A, 0x21, 0x26,  //   Usage (0x2621)
-    0x95, 0x08,        //   Report Count (8)
-    0x91, 0x02,        //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0x81, 0x01,        //   Input (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
     0xC0,              // End Collection
 };
 
@@ -523,10 +521,10 @@ const our_descriptor_def_t our_descriptors[] = {
     },
     {
         .idx = 2,
-        .descriptor = our_report_descriptor_gamepad,
-        .descriptor_length = sizeof(our_report_descriptor_gamepad),
+        .descriptor = our_report_descriptor_horipad,
+        .descriptor_length = sizeof(our_report_descriptor_horipad),
         .vid = 0x0F0D,
-        .pid = 0x0092,
+        .pid = 0x00C1,
         .handle_received_report = do_handle_received_report,
     },
     {
