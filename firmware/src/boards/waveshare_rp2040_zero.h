9125719cdbd5132803822bf77c9361e05c4c56cd
Muhammad Ibrahim Kahfie <ibrahimkahfie@gmail.com>
2024-11-03 08:40:45 +0700

    init rp2040_zero board, fix wrong uart pins for zero boards

---
+++ firmware/src/boards/waveshare_rp2040_zero.h
@@ -0,0 +1,36 @@
+// -----------------------------------------------------
+// NOTE: THIS HEADER IS ALSO INCLUDED BY ASSEMBLER SO
+//       SHOULD ONLY CONSIST OF PREPROCESSOR DIRECTIVES
+// -----------------------------------------------------
+#ifndef _BOARDS_WAVESHARE_RP2040_ZERO_H
+#define _BOARDS_WAVESHARE_RP2040_ZERO_H
+
+#define WAVESHARE_RP2040_ZERO_BOARD
+
+#define SERIAL_TX_PIN 8
+#define SERIAL_RX_PIN 9
+#define SERIAL_CTS_PIN 10
+#define SERIAL_RTS_PIN 11
+
+#ifndef PICO_DEFAULT_PIO_USB_DP_PIN 
+#define PICO_DEFAULT_PIO_USB_DP_PIN 0
+
+#define GPIO_VALID_PINS_BASE 0b00111111111111111111111111111111
+
+// --- FLASH ---
+
+#define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
+
+#ifndef PICO_FLASH_SPI_CLKDIV
+#define PICO_FLASH_SPI_CLKDIV 2
+#endif
+
+#ifndef PICO_FLASH_SIZE_BYTES
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
+#endif
+
+#ifndef PICO_RP2040_B0_SUPPORTED
+#define PICO_RP2040_B0_SUPPORTED 1
+#endif
+
+#endif
