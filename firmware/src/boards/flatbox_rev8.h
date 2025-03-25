f903c3443d1980bfd23b490841366e9789044e71
Jacek Fedorynski <jfedor@jfedor.org>
2025-03-25 18:00:41 +0100

    Firmware builds for Flatbox rev4, rev8

---
+++ firmware/src/boards/flatbox_rev8.h
@@ -0,0 +1,37 @@
+// -----------------------------------------------------
+// NOTE: THIS HEADER IS ALSO INCLUDED BY ASSEMBLER SO
+//       SHOULD ONLY CONSIST OF PREPROCESSOR DIRECTIVES
+// -----------------------------------------------------
+
+// pico_cmake_set PICO_PLATFORM=rp2350
+
+#ifndef _BOARDS_FLATBOX_REV8_H
+#define _BOARDS_FLATBOX_REV8_H
+
+#define FLATBOX_REV8_BOARD
+
+#define GPIO_VALID_PINS_BASE 0b00111111110011111111111111111111
+
+#define PICO_DEFAULT_PIO_USB_DP_PIN 20
+
+// --- RP2350 VARIANT ---
+#define PICO_RP2350A 1
+
+// --- FLASH ---
+
+#define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
+
+#ifndef PICO_FLASH_SPI_CLKDIV
+#define PICO_FLASH_SPI_CLKDIV 2
+#endif
+
+// pico_cmake_set_default PICO_FLASH_SIZE_BYTES = (16 * 1024 * 1024)
+#ifndef PICO_FLASH_SIZE_BYTES
+#define PICO_FLASH_SIZE_BYTES (16 * 1024 * 1024)
+#endif
+
+#ifndef PICO_RP2350_A2_SUPPORTED
+#define PICO_RP2350_A2_SUPPORTED 1
+#endif
+
+#endif
